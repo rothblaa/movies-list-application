43ffab41bd2889ec87ac7852025e015937d94975
rothblaa <adam.j.rothblatt@outlook.com>
2024-10-08 03:37:27 -0500

    added lifecycle hooks and using service

--- src/app/movie-list/movie-list.component.ts
+++ src/app/movie-list/movie-list.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { MovieService } from './../movie.service';
+import { Component, OnInit } from '@angular/core';
 import { Movie } from '../movie';
 import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-movie-list',
@@ -9,11 +11,21 @@ import { CommonModule } from '@angular/common';
   templateUrl: './movie-list.component.html',
   styleUrl: './movie-list.component.css'
 })
-export class MovieListComponent {
-  movies: Movie[] = [
-    {id: 1, title: "Movie One", director: "Director One", rating: 10},
-    {id: 2, title: "Movie Two", director: "Director Two", rating: 9},
-    {id: 3, title: "Movie Three", director: "Director Three", rating: 8},
-    {id: 4, title: "Movie Four", director: "Director Two", rating: 7}
-  ]
+export class MovieListComponent implements OnInit {
+
+  movies: Movie[] = []
+
+  constructor(private movieService: MovieService) {
+
+  }
+
+  ngOnInit(): void {
+    this.getMovies();
+  }
+
+  getMovies(): void {
+    this.movieService.getMovies()
+    .subscribe(moviesFromApi => this.movies = moviesFromApi)
+  }
+
 }

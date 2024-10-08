33a5aa8869468c621a35534914fdb45df2e7f86c
rothblaa <adam.j.rothblatt@outlook.com>
2024-10-08 03:03:26 -0500

    build movie service, send get request with obs to api

--- src/app/movie.service.ts
+++ src/app/movie.service.ts
@@ -1,9 +1,21 @@
+import { Movie } from './movie';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class MovieService {
 
-  constructor() { }
+  //not good practice to put this here, but just for simplicity of small app....
+  private apiUrl = 'https://localhost:7296';
+
+  constructor(private http : HttpClient) { }
+
+  // returns obs of type movie array; gets value asynchronously after subscribing to obs
+  getMovies(): Observable<Movie[]> {
+    return this.http.get<Movie[]>(this.apiUrl);
+  }
 }
